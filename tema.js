3ad80659d42332717fd2d9a13ee9be55a00a37c4
Giovana <62674318+Gigi17@users.noreply.github.com>
2020-10-09 11:30:52 -0300

    Add files via upload

---
+++ tema.js
@@ -0,0 +1,10 @@
+ function change() {
+    var element = document.body;
+    element.classList.toggle("extra-mode");
+ }
+ function myFunction() {
+    var element = document.body;
+    element.classList.toggle("contrast-mode");
+ }
+
+
